c5d563e68604a067281e6eb5a0a24bbceebea6a6
Md Roknuzzaman Rasel <rasel@graduate.utm.my>
2025-03-06 14:30:01 +0600

    hero section done

--- src/components/heroSection/Card.jsx
+++ src/components/heroSection/Card.jsx
@@ -9,21 +9,33 @@ const Card = ({title, description}) => {
     const firstHalf = words.slice(0, half).join(" ");
     const secondHalf = words.slice(half).join(" ");
   return (
-    <div className="text-white h-70 w-[440px] bg-black/20 backdrop-blur-md p-7 rounded-xl flex flex-col justify-between">
-      <div>
-        <h2 className="font-bold text-xl pb-3">
-          {firstHalf}
-          <br />
-          {secondHalf}
-        </h2>
-        <p className="text-m">{description}</p>
+    <div className="text-white  min-w-full xl:w-[440px] bg-black/20 backdrop-blur-md py-5  rounded-xl flex lg:flex-col justify-between ">
+      
+
+      <div className="flex items-center lg:flex-col gap-3 lg:gap-0">
+        <div className="lg:hidden">
+          <img className="h-20" src={AiIcon} alt="" />
+        </div>
+        <div>
+          <h2 className="font-bold text-xl lg:pb-5">
+            {firstHalf}
+            <br />
+            {secondHalf}
+          </h2>
+          <p className="text-m lg:pb-5">{description}</p>
+        </div>
       </div>
 
-      <div className="flex justify-between items-center pt-5">
+      <div className="hidden lg:flex  justify-between px-10 items-center pt-5">
         <img className="h-20" src={AiIcon} alt="" />
 
         <div className="bg-white/20 p-1 rounded-full">
           <img className="h-7" src={ArrowIcon} alt="" />
+        </div>
+      </div>
+      <div className="flex items-center lg:hidden justify-between pr-2 lg:pr-0">
+        <div className="bg-white/20 w-8 h-8 flex items-center justify-center rounded-full">
+          <img className="h-4 w-4" src={ArrowIcon} alt="" />
         </div>
       </div>
     </div>
--- src/components/heroSection/Hero.jsx
+++ src/components/heroSection/Hero.jsx
@@ -15,98 +15,80 @@ const Hero = () => {
   const cardData = [
     {
       title: "Staff Augmentation",
-      description: "Enabling your team to reach project goals."
+      description: "Enabling your team to reach project goals.",
     },
     {
       title: "Custom Software Development",
-      description: "Build software tailored to your business"
+      description: "Build software tailored to your business",
     },
     {
       title: "AI development",
-      description: "Get expert advice to make informed decisions"
+      description: "Get expert advice to make informed decisions",
     },
-  ]
-    return (
-      <>
-        <div
-          className="hero relative min-h-screen hero-section mb-5"
-          // style={{
-          //   backgroundImage:
-          //     "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-          // }}
-        >
-          <video
-            src={bgVideo}
-            autoPlay
-            muted
-            loop
-            className="absolute top-0 left-0 w-full h-full object-cover -z-10"
-          />
-          <div className="hero-overlay"></div>
-          <div className="hero-content flex items-start  justify-start ">
-            <div>
-              <div className="pb-2">
-                <p className="text-white flex pl-3 py-0.5 gap-1 max-w-[400px] border rounded-full">
-                  <span>
-                    <img className="h-6" src={BridgeIcon} alt="" />
-                  </span>{" "}
-                  Based in <span className="font-bold">Silicon Valley</span>.
-                  operate <span className="font-bold">worldwide</span>{" "}
-                </p>
-              </div>
-              <h1 className="pb-7 text-6xl text-white font-bold">
-                Empower your business <br /> with{" "}
-                <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#FAA818] via-[#E0403B] to-[#CE22E0]">
-                  top-class software development
-                </span>{" "}
-              </h1>
+  ];
+  return (
+    <div className="hero relative min-h-screen hero-section mb-5">
+      <video
+        src={bgVideo}
+        autoPlay
+        muted
+        loop
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+      />
 
-              {/* Card */}
-              <div className="w-full  gap-4 grid grid-cols-3">
-                {cardData.map((data, index) => (
-                  <Card
-                    key={index}
-                    title={data.title}
-                    description={data.description}
-                  />
-                ))}
-              </div>
+      <div className="hero-content flex w-full h-full justify-start  items-start">
+        <div>
+          <div className="pb-2 mt-12 lg:mt-20">
+            <p className="inline-flex items-center text-white px-3 py-0.5 gap-3  border rounded-full">
+              <img className="h-6" src={BridgeIcon} alt="" />
+              <span>
+                Based in <span className="font-bold">Silicon Valley</span>.
+                Operate <span className="font-bold">worldwide</span>
+              </span>
+            </p>
+          </div>
+          <h1 className="pb-7 text-xl sm:text-3xl md:text-4xl lg:text-6xl text-white font-bold">
+            Empower your business <br /> with{" "}
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#FAA818] via-[#E0403B] to-[#CE22E0]">
+              top-class software development
+            </span>{" "}
+          </h1>
 
-              {/* logo */}
+          <div className="absolute bottom-0 left-0 right-0">
+            {/* Card */}
+            <div className="max-w-[1350px] mx-auto gap-1 lg:gap-4 grid grid-cols-1 lg:grid-cols-3  mb-26 lg:mb-25 px-2 lg:px-0">
+              {cardData.map((data, index) => (
+                <Card
+                  key={index}
+                  title={data.title}
+                  description={data.description}
+                />
+              ))}
+            </div>
 
-              <div className="hero-overlay bg-black/20 backdrop-blur-md h-18 absolute bottom-0 left-0 right-0 flex items-center">
-                <div className="flex justify-between items-center max-w-[1350px] mx-auto w-full">
-                  <h1 className="text-white text-sm font-semibold leading-tight max-w-30">
-                    TRUSTED BY LEADING BRANDS AND STARTUPS
-                  </h1>
-                  <div className="flex gap-1">
-                    <img src={AmericanExpressLogo} alt="" />
-                    <img src={MultiverseLogo} alt="" />
-                    <img src={WholeFoodMarketLogo} alt="" />
-                    <img src={StarbucksLogo} alt="" />
-                    <img src={MarsLogo} alt="" />
-                    <img src={HiltonLogo} alt="" />
-                    <img src={ChemtreatLogo} alt="" />
-                  </div>
+            {/* logo */}
+
+            <div className="hero-overlay bg-black/20 backdrop-blur-md h-25 lg:h-18 absolute bottom-0 left-0 right-0 flex items-center">
+              <div className="flex flex-col lg:flex-row justify-between items-center max-w-[1350px] mx-auto w-full">
+                <h1 className="text-white text-sm font-semibold leading-tight lg:max-w-30">
+                  TRUSTED BY LEADING BRANDS AND STARTUPS
+                </h1>
+                <div className="flex  overflow-x-auto whitespace-nowrap no-scrollbar">
+                  <img src={AmericanExpressLogo} alt="" />
+                  <img src={MultiverseLogo} alt="" />
+                  <img src={WholeFoodMarketLogo} alt="" />
+                  <img src={StarbucksLogo} alt="" />
+                  <img src={MarsLogo} alt="" />
+                  <img src={HiltonLogo} alt="" />
+                  <img src={ChemtreatLogo} alt="" />
                 </div>
               </div>
             </div>
           </div>
-
-          {/* <div className="hero-content text-neutral-content min-w-[90%] border ">
-            <div className="max-w-full border">
-              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-              <p className="mb-5">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-              <button className="btn btn-primary">Get Started</button>
-            </div>
-          </div> */}
         </div>
-      </>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
