bb594df227824c9b92b06d32fae8bdf50ed6f7b7
Md Roknuzzaman Rasel <rasel@graduate.utm.my>
2025-03-07 23:59:33 +0600

    coustom developmentsection update

--- src/components/CustomDevelopmetSection/CustomDevelopmentSection.jsx
+++ src/components/CustomDevelopmetSection/CustomDevelopmentSection.jsx
@@ -3,13 +3,21 @@ import CustomDevelopmentCard from "./CustomDevelopmentCard";
 const CustomDevelopmentSection = () => {
   return (
     <div className="bg-[#040D12] p-10">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5 max-w-[1350px] mx-auto ">
-        <CustomDevelopmentCard />
-        <CustomDevelopmentCard />
-        <CustomDevelopmentCard />
-        <CustomDevelopmentCard />
-        <CustomDevelopmentCard />
-        <CustomDevelopmentCard />
+      <div className="max-w-[1350px] mx-auto ">
+        <h1 className="text-white text-2xl md:text-4xl font-bold text-center pb-15">
+          Tailored Custom Development <br /> Solutions{" "}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#FAA818] via-[#E0403B] to-[#CE22E0]">
+            for Your Business
+          </span>
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5 ">
+          <CustomDevelopmentCard />
+          <CustomDevelopmentCard />
+          <CustomDevelopmentCard />
+          <CustomDevelopmentCard />
+          <CustomDevelopmentCard />
+          <CustomDevelopmentCard />
+        </div>
       </div>
     </div>
   );
